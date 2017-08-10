1f0c9a27aeeddca5647c11c0ea0b3838a9862c37
Shashank Saudagar <shashank@ajency.in>
2017-08-10 13:25:13 +0530

    slug increment fixed

--- app/Listing.php
+++ app/Listing.php
@@ -76,6 +76,7 @@ class Listing extends Model
             do {
                 $slug1 = $slug . '-' . $i;
                 $count = Listing::where('slug', $slug1)->count();
+                i++;
             } while ($count > 0);
         }
 

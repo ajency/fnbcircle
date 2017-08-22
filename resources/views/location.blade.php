4e73fb2b523a008fcabd627cdfd4bc7b7a44a5ad
Ajaj Rajguru <ajaj@ajency.in>
2017-08-21 17:38:34 -0800

    half modal loader added

--- resources/views/location.blade.php
+++ resources/views/location.blade.php
@@ -317,7 +317,19 @@
                             @endforeach
                         </ul>
                         <!-- Tab panes -->
-                        <div class="tab-content">
+                        <div class="tab-content relative">
+                            <div class="site-loader section-loader half-modal hidden">
+                               <div id="floatingBarsG">
+                                    <div class="blockG" id="rotateG_01"></div>
+                                    <div class="blockG" id="rotateG_02"></div>
+                                    <div class="blockG" id="rotateG_03"></div>
+                                    <div class="blockG" id="rotateG_04"></div>
+                                    <div class="blockG" id="rotateG_05"></div>
+                                    <div class="blockG" id="rotateG_06"></div>
+                                    <div class="blockG" id="rotateG_07"></div>
+                                    <div class="blockG" id="rotateG_08"></div>
+                                </div>
+                            </div>
                             <!-- mobile collapse -->
                             @foreach($cities as $city)
                             <div class="toggle-collapse desk-hide" data-toggle="collapse" data-target="#{{$city->slug}}" aria-expanded="false" aria-controls="{{$city->slug}}">

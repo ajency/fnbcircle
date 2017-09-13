a91eeefa81185ed605f16d66a48f5ee983fbe4f2
Ajaj Rajguru <ajaj@ajency.in>
2017-09-13 14:56:40 +0530

    add more area added

--- public/js/custom.js
+++ public/js/custom.js
@@ -422,7 +422,7 @@ $(function(){
 		// });
 
 		// Bootstrap multiselect
-		if($('.multi-select').length){
+		if($('.multi-select,.location-select').length){
 			$('.multi-select').multiselect({
 	            includeSelectAllOption: true,
 	            numberDisplayed: 1
@@ -473,6 +473,11 @@ $(function(){
 		    });
 		    area_group_clone.insertBefore(area_group);
   		});
+
+		$('body').on('click', '.remove-select-col', function() {
+  			$(this).closest('.location-select').remove();
+  		});
+
 
 		$('input[type=radio][name=plan-select]').change(function() {
 		  if ($(this).is(':checked')) {
@@ -628,6 +633,72 @@ $(function(){
 		});
 		var infoIcon = '<i class="fa fa-info-circle p-l-5" aria-hidden="true"></i>'
 		$('.multipleOptions .multiselect-container li:not(.multiselect-all) .checkbox').append(infoIcon);
+
+
+
+
+		/* exported initSample */
+
+		if ( CKEDITOR.env.ie && CKEDITOR.env.version < 9 )
+			CKEDITOR.tools.enableHtml5Elements( document );
+
+		// The trick to keep the editor in the sample quite small
+		// unless user specified own height.
+		CKEDITOR.config.height = 150;
+		CKEDITOR.config.width = 'auto';
+
+		var initSample = ( function() {
+			var wysiwygareaAvailable = isWysiwygareaAvailable(),
+				isBBCodeBuiltIn = !!CKEDITOR.plugins.get( 'bbcode' );
+
+			return function() {
+				var editorElement = CKEDITOR.document.getById( 'editor' );
+
+				// :(((
+				if ( isBBCodeBuiltIn ) {
+					editorElement.setHtml(
+						'Hello world!\n\n' +
+						'I\'m an instance of [url=http://ckeditor.com]CKEditor[/url].'
+					);
+				}
+
+				// Depending on the wysiwygare plugin availability initialize classic or inline editor.
+				if ( wysiwygareaAvailable ) {
+					CKEDITOR.replace( 'editor' );
+				} else {
+					editorElement.setAttribute( 'contenteditable', 'true' );
+					CKEDITOR.inline( 'editor' );
+
+					// TODO we can consider displaying some info box that
+					// without wysiwygarea the classic editor may not work.
+				}
+			};
+
+			function isWysiwygareaAvailable() {
+				// If in development mode, then the wysiwygarea must be available.
+				// Split REV into two strings so builder does not replace it :D.
+				if ( CKEDITOR.revision == ( '%RE' + 'V%' ) ) {
+					return true;
+				}
+
+				return !!CKEDITOR.plugins.get( 'wysiwygarea' );
+			}
+		} )();
+
+
+		initSample();
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
 

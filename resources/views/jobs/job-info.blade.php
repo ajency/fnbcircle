82e4b29ec105862ca8ac2b8b58f1d41b36b44bfc
Ajaj Rajguru <ajaj@ajency.in>
2017-09-15 14:34:56 +0530

    #299 - updated

--- resources/views/jobs/job-info.blade.php
+++ resources/views/jobs/job-info.blade.php
@@ -59,7 +59,7 @@
     <!-- Job keywords -->
 
     <div class="m-t-40 c-gap">
-        <label class="label-size">Keywords</label>
+        <label class="label-size">Keywords :</label>
         <div class="m-t-5 flex-data-row">
             <input type="text" class="form-control fnb-input job-keywords" name="job_keyword" placeholder="Type and hit enter" list="jobKeyword" multiple="multiple" id=jobKeywordInput @if(isset($job['meta_data']['job_keyword']) && !empty($job['meta_data']['job_keyword'])) value='{{ implode(",",$job['meta_data']['job_keyword']) }}' @endif  >
 

f3d95268f8b8de00ccead6061d8a32cba9415c5b
SharathSkN <sharath@ajency.in>
2017-09-18 13:47:03 +0530

    Added Login / Logout in fnbtemplate.blade.php

--- resources/views/admin-dashboard/internal_users.blade.php
+++ resources/views/admin-dashboard/internal_users.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.admin-dashboard')
 
+@section('css')
+    <!-- Datatables -->
+    <link href="/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css" rel="stylesheet">
+    <link href="/css/bootstrap-multiselect.min.css" rel="stylesheet">
+    <!-- Main styles -->
+    <link href="/css/dashboard.css" rel="stylesheet">
+@endsection
+
 @section('js')
   @parent
   <script type="text/javascript" src="/bower_components/bootstrap-confirmation2/bootstrap-confirmation.min.js"></script>

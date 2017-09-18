a473d846e27f0d98ef531655c7415d84f0f34d6b
SharathSkN <sharath@ajency.in>
2017-09-18 07:44:24 +0530

    Remove console print statement

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -87,7 +87,6 @@ class LoginController extends Controller
         if ($valid_response["status"] == "success") {
             if($valid_response["authentic_user"]) {
                 $userdata_response = $userauth_obj->getUserData($valid_response["user"], false);
-                $output->writeln("Response: " . json_encode($userdata_response));
                 return $fnbauth_obj->rerouteUser(array("user" => $valid_response["user"], "status" => "success", "filled_required_status" => $userdata_response["required_fields_filled"]), "website");
 
             } else {

c9f685e9a9be6e6ed315f8a465232351df91e298
SharathSkN <sharath@ajency.in>
2017-09-18 12:36:27 +0530

    Added internal_users & registered_users pages

---
+++ resources/views/admin-dashboard/internal_users.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.admin-dashboard')
+
+@section('js')
+  @parent
+  <script type="text/javascript" src="/bower_components/bootstrap-confirmation2/bootstrap-confirmation.min.js"></script>
+@endsection
+
+@section('page-data')
+
+@section

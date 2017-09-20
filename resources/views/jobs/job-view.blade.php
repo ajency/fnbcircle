85399835891c564e9798becc0d793817764fe8ac
Prajay Verenkar <prajay@ajency.in>
2017-09-20 16:48:09 +0530

    no message

--- resources/views/jobs/job-view.blade.php
+++ resources/views/jobs/job-view.blade.php
@@ -172,21 +172,7 @@
                               @endif
                            </ul>
                         </div>
-
-                        <div class="opertaions__container flex-row job-location">
-                           <div class="location flex-row">
-                               <span class="fnb-icons map-icon"></span>
-                               <p class="default-size location__title c-title">Mumbai <i class="fa fa-caret-right p-l-5" aria-hidden="true"></i></h6>
-                           </div>
-                           <ul class="cities flex-row">
-                               <li>
-                                   <p class="cities__title">Bandra, </p>
-                               </li>
-                               <li>
-                                   <p class="cities__title">Andheri, </p>
-                               </li>
-                           </ul>
-                        </div>                            
+                           
 
                       </div>
   

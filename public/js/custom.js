5d79f39dd1f68cdf1e0bf3bd62bb9a3b256250cb
SharathSkN <sharath@ajency.in>
2017-09-22 14:41:29 +0530

    Updated the /get-areas code - functioning in Listing

--- public/js/custom.js
+++ public/js/custom.js
@@ -406,7 +406,7 @@ $(function(){
 					success: function(data) {
 						var key;
 						for (key in data) {
-							html += '<option value="' + key + '">' + data[key] + '</option>';
+							html += '<option value="' + data[key]["id"] + '">' + data[key]["name"] + '</option>';
 						}
 						$("#" + parent + ' select[name="area"]').html(html);
 					},

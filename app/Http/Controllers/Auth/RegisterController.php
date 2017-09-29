f5193bbdd3baf79122c1851a1e792029ec47355d
SharathSkN <sharath@ajency.in>
2017-09-29 13:50:37 +0530

    #431

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -88,9 +88,15 @@ class RegisterController extends Controller
 
         $request_data = [
             "user" => array("username" => $request->email, "email" => $request->email, "name" => $request->name),
-            "user_comm" => array("object_type" => "App\User", "email" => $request->email, "is_primary" => 1, "is_communication" => 1, "is_verified" => ($request->has('is_contact_verified') ? $request->is_contact_verified : 0), "is_visible" => 0),
+            "user_comm" => array("object_type" => "App\User", "email" => $request->email, "is_primary" => 1, "is_communication" => 1, "is_visible" => 0),
             "user_details" => array("is_job_seeker" => 0, "has_job_listing" => 0, "has_business_listing" => 0, "has_restaurant_listing" => 0)
         ];
+
+        if($request->has('is_contact_verified') && in_array(strtolower($request->is_contact_verified), ['true', '1'])) {
+            $request_data["user_comm"]["is_verified"] = 1;
+        } else {
+            $request_data["user_comm"]["is_verified"] = 0;
+        }
 
         if($request->has("next_url"))  {
             $next_redirect_url = $request->next_url;

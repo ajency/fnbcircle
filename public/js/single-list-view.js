e205e1f7fb893227786ea085438500620177c656
Shashank Shetye Saudagar <shashank@ajency.in>
2017-10-06 14:07:34 +0530

    Share icons added

---
+++ public/js/single-list-view.js
@@ -0,0 +1,8 @@
+(function() {
+  $('#shareRoundIcons').jsSocials({
+    showLabel: false,
+    showCount: false,
+    shares: ['email', 'twitter', 'facebook', 'googleplus', 'linkedin', 'pinterest', 'stumbleupon', 'whatsapp']
+  });
+
+}).call(this);

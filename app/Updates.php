81cbcda23b56f6e416c7a4c72cbc495a9710d62f
Shashank Shetye Saudagar <shashank@ajency.in>
2017-10-10 12:37:31 +0530

    Migrations

---
+++ app/Updates.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Ajency\FileUpload\FileUpload;
+use Illuminate\Database\Eloquent\Model;
+
+class Updates extends Model
+{
+    use FileUpload;
+
+
+}
---
+++ database/migrations/2017_10_10_122436_create_updates_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUpdatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('updates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('object_type');
+            $table->integer('object_id');
+            $table->integer('posted_by');
+            $table->integer('last_updated_by');
+            $table->string('title');
+            $table->text('contents');
+            $table->string('photos');
+            $table->softDeletes();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('updates');
+    }
+}

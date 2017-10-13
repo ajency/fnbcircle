3f8a1a9bc262611fac00b50ddd8dd02458ef7b86
Prajay Verenkar <prajay@ajency.in>
2017-10-13 10:59:22 +0530

    no message

--- app/FnbPermissions.php
+++ app/FnbPermissions.php
@@ -33,6 +33,7 @@ function routePermission(){
 			 	//ui element
 				'submit_review_element_cls'=>['submit_for_review_job','submit_for_review_listing'],
 				'edit_permission_element_cls'=>['edit_job','edit_listing'],
+				'change_status_element_cls'=>['job_status'],
 			],
 
 		'normal_user_check' =>
@@ -136,7 +137,7 @@ function isOwner($table,$referenceKey,$userReferenceKey,$referenceId){
 		$result = false;
 	
 
-	return 
+	return $result;
 }
 
 /***

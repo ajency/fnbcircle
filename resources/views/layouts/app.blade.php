f707c783a46db2f3ff062938691b30af752b3340
Prajay Verenkar <prajay@ajency.in>
2017-10-19 19:30:52 +0530

    no message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link rel="canonical" href="{{ url('/')}}" />
     @yield('meta') 
     <link rel="shortcut icon" href="/img/logo-fnb.png" />
 
-    @yield('openGraph')
+    @yield('seo')
     <!-- Google font cdn -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
     <!-- Font awesome cdn -->
--- resources/views/layouts/single-view.blade.php
+++ resources/views/layouts/single-view.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.app')
 @section('content')
 <!-- content -->
+@section('seo')
+	@yield('openGraph')
+@endsection
+
   <div class="single-view-head">
     @yield('single-view-data')
   </div>

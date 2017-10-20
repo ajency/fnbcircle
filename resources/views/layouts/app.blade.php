4478bc542f16f0dd2b5455107f97325bcc381c34
Prajay Verenkar <prajay@ajency.in>
2017-10-20 11:56:46 +0530

    no message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    @yield('meta') 
     <link rel="shortcut icon" href="/img/logo-fnb.png" />
     <!-- <title>Homepage</title> -->
-    <title> @yield('title')</title>
     @yield('openGraph')
     <!-- Google font cdn -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">

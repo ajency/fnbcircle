dd09c830dfcbd25d0a5b49059108dc5114ff9da4
Shashank Shetye Saudagar <shashank@ajency.in>
2017-10-20 19:48:29 +0530

    #802

--- app/Http/Controllers/AdminModerationController.php
+++ app/Http/Controllers/AdminModerationController.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\ListingController;
 use App\Listing;
 use App\ListingCategory;
 use App\ListingCommunication;
+use App\PlanAssociation;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\User;
@@ -122,6 +123,14 @@ class AdminModerationController extends Controller
             $users  = User::whereIn('type',$filters["updated_by"]['user_type'])->pluck('id')->toArray();
             $listings = $listings->whereIn('last_updated_by',$users);
         }
+        if(isset($filters["premium"])){
+            $request_senders = array_unique(PlanAssociation::where('premium_type','App\\Listing')->pluck('premium_id')->toArray());
+           $prem = [];
+           if(count($filters["premium"]) == 1){
+            if($filters["premium"][0] == 1) $listings->whereIn('id',$request_senders);
+            if($filters["premium"][0] == 0) $listings->whereNotIn('id',$request_senders);
+           } 
+        }
         if(isset($filters['type'])){
             $listings = $listings->where(function ($listings) use ($filters){
                 foreach($filters['type'] as $type ){
@@ -181,7 +190,7 @@ class AdminModerationController extends Controller
             // }
             $dup                                  = $this->getDuplicateCount($listing->id, $listing->title);
             $response[$listing->id]['duplicates'] = $dup['phone'] . ',' . $dup['email'] . ',' . $dup['title'];
-            $response[$listing->id]['premium']    = 'No';
+            $response[$listing->id]['premium']    = (count($listing->premium()->get())>0)? "Yes":"No";
             $response[$listing->id]['categories'] = ListingCategory::getCategories($listing->id);
             if($listing->owner == null) $response[$listing->id]['type'] = 'orphan';
             else $response[$listing->id]['type'] = 'verified';

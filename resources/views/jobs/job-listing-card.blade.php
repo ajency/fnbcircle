3d9b426e80ffefa7892942fc3796dc00060e4a4b
Ajaj Rajguru <ajaj@ajency.in>
2017-10-26 16:22:53 +0530

    code updated

--- resources/views/jobs/job-listing-card.blade.php
+++ resources/views/jobs/job-listing-card.blade.php
@@ -13,11 +13,11 @@
                         <a href="{{ url('/job/'.$job->getJobSlug()) }}" target="_blank" class="btn fnb-btn full primary-btn border-btn fullwidth default-size">View Job <i class="fa fa-arrow-right p-l-5" aria-hidden="true"></i></a>
                       </div>
 
-                        @if(isset($showApplication) && $showApplication)
-                  <div class="get-details detail-move">
-                      <a href="#" class="apply-jobs" data-toggle="modal" data-target="#job-application-{{ $job->id}}"><button class="btn fnb-btn outline full border-btn fullwidth default-size">View Application <i class="fa fa-arrow-right p-l-5" aria-hidden="true"></i></button></a>
-                  </div>
-                  @endif
+                    @if(isset($showApplication) && $showApplication)
+                    <div class="get-details detail-move">
+                        <a href="#" class="apply-jobs" data-toggle="modal" data-target="#job-application-{{ $job->id}}"><button class="btn fnb-btn outline full border-btn fullwidth default-size">View Application <i class="fa fa-arrow-right p-l-5" aria-hidden="true"></i></button></a>
+                    </div>
+                    @endif
                   
                   </div>
                   <div class="location flex-row companyName">
@@ -131,60 +131,11 @@
               </div>
           </div>
  
-          <div class="body-right flex-cols">
-              <div class="operations">
-                  <img src="{{ asset('/img/power-seller.png') }}" class="img-responsive power-seller" width="120">
-                  <p class="operations__title default-size text-lighter m-t-5">Job Location:</p>
-                  @foreach($job->getJobLocationNames() as $city => $locAreas)
-                  <div class="operations__container">
-                      <div class="location flex-row">
-                          <p class="m-b-0 text-color heavier default-size">{{ $city }} <i class="fa fa-caret-right p-l-5" aria-hidden="true"></i>
-                          </p>
-                      </div>
-                      <ul class="cities flex-row">
-                        @php
-                              $splitAreas =  splitJobArrayData($locAreas,2);
-                              $areas = $splitAreas['array'];
-                              $moreAreas = $splitAreas['moreArray'];
-                              $moreAreaCount = $splitAreas['moreArrayCount'];
-                              $areaCount = count($areas);
-                              $areaInc = 0;
-                        @endphp 
-                          @foreach($areas as $area)
-                            @php
-                                 $areaInc++;
-                            @endphp 
-                          <li>
-                              <p class="cities__title default-size">{{ $area }}
-                                @if($areaInc != $areaCount)
-                                   , 
-                                  @endif
-                               </p>
-                          </li>
-                          @endforeach  
-                          
-                          @if($moreAreaCount) 
-                          <li class="line">
-                              <p class="cities__title default-size">|</p>
-                          </li>
-                          <li class="remain more-show">
-                              <a href="" class="cities__title remain__number default-size text-medium">more...</a>
-                          </li>
-                        @endif
-                      </ul>
-                  </div>
-                  @endforeach  
-
-              </div>
-              <div>
-         
-              </div>
- 
           <div class="recent-updates open-border">
             <p class="operations__title default-size grey-darker heavier m-t-0">Job Description</p>
             <p class="m-t-0 heavier text-lighter text-medium default-size job-list-desc">{{ $job->getShortDescription() }}</p>
- 
           </div>
+
            <div class="recent-updates flex-row flex-wrap open-border">
              <div class="off-salary">
                 <p class="operations__title default-size grey-darker heavier m-t-0">Offered Salary</p>

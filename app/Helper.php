c28ebcf84e334752bd97e4d8e793259a02fdc033
Shashank Shetye Saudagar <shashank@ajency.in>
2017-10-31 12:53:13 +0530

    attachments added

--- app/Helper.php
+++ app/Helper.php
@@ -329,6 +329,10 @@ function generateUrl($city, $slug, $slug_extra = []) {
 *	@param from
 *	@param name
 * 	@param subject
+*	@param attach - An Array of arrays each containing the following parameters:
+*			@param file - base64 encoded raw file
+*			@param as - filename to be given to the attachment
+*			@param mime - mime of the attachment
 */
 function sendEmail($event='new-user', $data=[]) {
 	$email = new \Ajency\Comm\Models\EmailRecipient();
@@ -351,6 +355,9 @@ function sendEmail($event='new-user', $data=[]) {
 	if(!is_array($params)) $params = [$params];
 	$params['email_subject'] = (isset($data['subject']))? $data['subject']:"";
 	$email->setParams($params);
+
+	if(isset($data['attach'])) $email->setAttachments($data['attach']);
+
 	$notify = new \Ajency\Comm\Communication\Notification();
     $notify->setEvent($event);
     $notify->setRecipientIds([$email]);

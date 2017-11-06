3f202ac48af4746258d1b16a8b80d651affc08d4
Amit <amit@ajency.in>
2017-11-06 11:04:21 +0530

    w

--- public/news/wp-content/themes/twentyseventeen/archive.php
+++ public/news/wp-content/themes/twentyseventeen/archive.php
@@ -12,42 +12,50 @@
 
 get_header(); ?>
 
-	<div id="content">
+<div class="wrap">
 
-        <?php query_posts('post_type=post&post_status=publish&posts_per_page=10&paged='. get_query_var('paged')); ?>
+	<?php if ( have_posts() ) : ?>
+		<header class="page-header">
+			<?php
+				the_archive_title( '<h1 class="page-title">', '</h1>' );
+				the_archive_description( '<div class="taxonomy-description">', '</div>' );
+			?>
+		</header><!-- .page-header -->
+	<?php endif; ?>
 
-	<?php if( have_posts() ): ?>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
 
-        <?php while( have_posts() ): the_post(); ?>
+		<?php
+		if ( have_posts() ) : ?>
+			<?php
+			/* Start the Loop */
+			while ( have_posts() ) : the_post();
 
-	    <div id="post-<?php get_the_ID(); ?>" <?php post_class(); ?>>
+				/*
+				 * Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/post/content', get_post_format() );
 
-        	<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( array(200,220) ); ?></a>
+			endwhile;
 
-                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			the_posts_pagination( array(
+				'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
+				'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
+				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
+			) );
 
-                <span class="meta"><?php author_profile_avatar_link(48); ?> <strong><?php the_time('F jS, Y'); ?></strong> / <strong><?php the_author_link(); ?></strong> / <span class="comments"><?php comments_popup_link(__('0 comments','example'),__('1 comment','example'),__('% comments','example')); ?></span></span>
+		else :
 
-		<?php the_excerpt(__('Continue reading »','example')); ?>
+			get_template_part( 'template-parts/post/content', 'none' );
 
-            </div><!-- /#post-<?php get_the_ID(); ?> -->
+		endif; ?>
 
-        <?php endwhile; ?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+	<?php get_sidebar(); ?>
+</div><!-- .wrap -->
 
-		<div class="navigation">
-			<span class="newer"><?php previous_posts_link(__('« Newer','example')) ?></span> <span class="older"><?php next_posts_link(__('Older »','example')) ?></span>
-		</div><!-- /.navigation -->
-
-	<?php else: ?>
-
-		<div id="post-404" class="noposts">
-
-		    <p><?php _e('None found.','example'); ?></p>
-
-	    </div><!-- /#post-404 -->
-
-	<?php endif; wp_reset_query(); ?>
-
-	</div><!-- /#content -->
-
-<?php get_footer(); ?>
+<?php get_footer();

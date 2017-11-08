1e57484ee482e5303defd518e3391f8981ac84ed
Ajaj Rajguru <ajaj@ajency.in>
2017-11-08 18:32:54 +0530

    #1069 - fixed

--- resources/views/email/user-verify.blade.php
+++ resources/views/email/user-verify.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.email')
 
 @section('content')
-<table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
+        <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
     <tr style="text-align: center;">
       <td style="padding: 30px; font-family: sans-serif; font-size: 16px; line-height: 24px; color: #555555;background-color: #fff;">
   
@@ -24,10 +24,10 @@
           To finish setting up this FnB Circle account, we need to make sure this email address is yours.<br>
 
             <a href="{{ $confirmationLink }}" style="background-color: #ec6d4b;color: #fff;padding: 8px;width: 120px;display: block;margin: 20px auto;text-align: center;text-decoration: none;" target="_blank">Verify email</a>
-            <b style="display:block;margin-bottom: 10px;">OR</b>
+            <b style="display:block;margin-bottom: 10px;color: #616161;">OR</b>
              Copy and paste this link into your browser: {{ $confirmationLink }}
- <br>
-
+          <br><br>
+          <b style="color: #616161;">NOTE</b> : The above link is valid only for 2 hours.<br><br><br>
            
             <br><br>
           <div style="text-align: left;">

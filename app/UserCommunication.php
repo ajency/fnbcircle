a137d5e00d9486fa1a1a6b3d36555acd691fe370
Unknown <sharath@ajency.in>
2017-11-09 15:34:48 +0530

    #1089, #1093

--- app/UserCommunication.php
+++ app/UserCommunication.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserCommunication extends Model
 {
+	/**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'object_id', 'object_type', 'type', 'value', 'country_code', 'is_primary' 'is_communication', 'is_verified', 'is_visible',
+    ];
 
 	public function object(){
 		return $this->morphTo();

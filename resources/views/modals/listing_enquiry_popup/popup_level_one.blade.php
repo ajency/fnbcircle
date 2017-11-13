cf35fe79a410293cd3e095ac3a83915b3ebcfab7
Ajaj Rajguru <ajaj@ajency.in>
2017-11-13 18:08:01 +0530

    #1122 - fixed

--- resources/views/modals/listing_enquiry_popup/popup_level_one.blade.php
+++ resources/views/modals/listing_enquiry_popup/popup_level_one.blade.php
@@ -92,9 +92,10 @@
         <div class="looking-for gap-separator">
             <a class="secondary-link text-decor desk-hide looking-for__toggle" data-toggle="collapse" href="#lookingfor" aria-expanded="false" aria-controls="lookingfor">Add a note</a>
             <div class="collapse in" id="lookingfor">
-                <p class="text-darker describes__title heavier">Tell the business owner what you're looking for</p>
+                <p class="text-darker describes__title heavier m-b-5">Give the supplier/service provider some details of your requirements</p>
+                <div class="text-lighter x-small heavier">You may specify product/services, quantities, specifications, your company/brand name, etc.</div>
                 <div class="form-group">
-                    <input type="text" class="form-control fnb-input" name="enquiry_message" placeholder="Type here....">
+                    <input type="text" class="form-control fnb-input" name="enquiry_message" placeholder="">
                 </div>
             </div>
         </div>

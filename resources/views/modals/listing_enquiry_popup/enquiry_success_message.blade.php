48ee1b67fbf110b2dd758ea68e10b84524f6e147
Ajaj Rajguru <ajaj@ajency.in>
2017-11-14 15:14:32 +0530

    no listing data condition

--- resources/views/modals/listing_enquiry_popup/enquiry_success_message.blade.php
+++ resources/views/modals/listing_enquiry_popup/enquiry_success_message.blade.php
@@ -3,6 +3,14 @@
         <i class="fa fa-check-circle" aria-hidden="true"></i>
         <h6 class="text-color text-medium enquiry-success__text" style="padding-right: 0em">Email &amp; SMS with your details has been sent to the relevant listing owners. You will be contacted soon.</h6>
     </div>
+
+	<!-- <div class="no-results">
+        <h5 class="seller-info__title ellipsis text-primary">Sorry, No Business Listings Matching Your Requirements! <i class="fa fa-frown-o" aria-hidden="true"></i></h3>
+        <img src="/img/404.png" class="img-reponsive center-block img-nf m-t-40" width="350">
+    </div> -->
+
+
+
     <div class="suppliers-data success-cards">
         <p class="element-title heavier text-darker success-cards__title">Don't miss out on these suppliers <img src="/img/direction-down-2.png" class="img-responsive direction-down"></p>
         <!-- enquiry success ends -->

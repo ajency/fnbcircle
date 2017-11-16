8ad672316aea04641d39bc52a0eb9a035051cd17
Ajaj Rajguru <ajaj@ajency.in>
2017-11-16 16:15:24 +0530

    #1164 - fixed

--- resources/views/email/listing_owner_direct.blade.php
+++ resources/views/email/listing_owner_direct.blade.php
@@ -16,30 +16,30 @@
 					There is an enquiry for <a href="#">{{ $listing_name }}</a><br><br>
 
 					Please find below details of the seeker.<br>
-					<label>Name:</label> {{ $customer_name }}<br>
-					<label>Email:</label>  {{ $customer_email }}<br>
-					<label>Phone Number:</label> {{ $customer_contact }}<br>
-					<label>What describes you the best?</label><br>
-						<ul>
-                            @php
-                                if(isset($customer_describes_best)) {
-                                    $describes_best_html = generateHTML("enquiry_popup_display", $customer_describes_best);
-                                } else {
-                                    $describes_best_html = generateHTML("enquiry_popup_display", []);
-                                }
-                            @endphp
-                            @foreach($describes_best_html as $index_best => $value_best)
-                                {!! $value_best["html"] !!}<br>
-                            @endforeach
-                        </ul>
-					<label>Message:</label> {{ $customer_message }}<br><br>
+					<b>Name:</b> {{ $customer_name }}<br>
+					<b>Email:</b>  {{ $customer_email }}<br>
+					<b>Phone Number:</b> {{ $customer_contact }}<br>
+					<b>What describes you the best?</b><br>
+					<ul style="margin: 0;padding-left: 10px;list-style: none;">
+                        @php
+                            if(isset($customer_describes_best)) {
+                                $describes_best_html = generateHTML("enquiry_popup_display", $customer_describes_best);
+                            } else {
+                                $describes_best_html = generateHTML("enquiry_popup_display", []);
+                            }
+                        @endphp
+                        @foreach($describes_best_html as $index_best => $value_best)
+                            {!! $value_best["html"] !!}
+                        @endforeach
+                    </ul>
+					<b>Message:</b> {{ $customer_message }}<br><br>
 
 					<a href="{{ $customer_dashboard_url }}" style="color: #ec6d4b;font-weight: 600;" target="_blank">Click here</a> to view the profile of {{ $customer_name }}.
 					<br>
 					You may now contact the seeker directly.<br><br>
 
 
-					Disclaimer: FnB Circle is only a intermediary platform between the business owners and seekers and hence shall neither be responsible nor liable to mediate or resolve any disputes or disagreements between the business owners and seekers.
+					<b>Disclaimer:</b> FnB Circle is only a intermediary platform between the business owners and seekers and hence shall neither be responsible nor liable to mediate or resolve any disputes or disagreements between the business owners and seekers.<br><br>
 						<div>
 	          				Regards,<br>
 	          				Team FnB Circle<br>

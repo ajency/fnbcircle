099e8c814e1cd1663a6e3e911be6fdee7cb76f5a
Ajaj Rajguru <ajaj@ajency.in>
2017-11-17 11:44:44 +0530

    search bar updated

--- public/news/wp-content/themes/twentyseventeen/index.php
+++ public/news/wp-content/themes/twentyseventeen/index.php
@@ -18,32 +18,42 @@
 
 get_header(); ?>
 <div class="header-image text-center">
-	<H1 class="bold-t" >FnB Circle News</H1>
 	<div class="search-container">
-		<!-- <select>
-		  <option value="volvo">Panjim</option>
-		  <option value="saab">Mumbai</option>
-		  <option value="mercedes">Kerala</option>
-		  <option value="audi">Pune</option>
-		</select> -->
-		 <label class="search-label home_city_select_label">
-		 <i class="fa fa-map-marker" aria-hidden="true"></i>
-		<?php wp_dropdown_categories('show_option_none=All City&exclude=1&value_field=slug'); ?>
-		</label>
-	<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-		
-		 <label>
-	        <span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
-	        <i class="fa fa-search" aria-hidden="true"></i>
-	        <input type="search" class="search-field"
-	            placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>"
-	            value="<?php echo get_search_query() ?>" name="s"
-	            title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
-	    </label>
-	    <input type="button" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
-	</form>
-	<div class="clear"></div>
-</div>
+			<!-- <select>
+			  <option value="volvo">Panjim</option>
+			  <option value="saab">Mumbai</option>
+			  <option value="mercedes">Kerala</option>
+			  <option value="audi">Pune</option>
+			</select> -->
+			 <label class="search-label home_city_select_label hidden">
+			 <i class="fa fa-map-marker" aria-hidden="true"></i>
+			<?php wp_dropdown_categories('show_option_none=All City&exclude=1&value_field=slug'); ?>
+			</label>
+		<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+			
+			 <label class="hidden">
+		        <span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
+		        <i class="fa fa-search" aria-hidden="true"></i>
+		        
+		    </label>
+		    <div class="expandSearch">
+		    	<input type="search" class="search-field custom-expand-search"
+		            placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>"
+		            value="<?php echo get_search_query() ?>" name="s"
+		            title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+		            <a href="#">
+						<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="20px" height="20px" viewBox="375.045 607.885 30.959 30.33" enable-background="new 375.045 607.885 30.959 30.33" xml:space="preserve">
+							<path fill="#494949" d="M405.047,633.805l-7.007-6.542c-0.129-0.121-0.267-0.226-0.408-0.319c1.277-1.939,2.025-4.258,2.025-6.753 c0-6.796-5.51-12.306-12.307-12.306s-12.306,5.51-12.306,12.306s5.509,12.306,12.306,12.306c2.565,0,4.945-0.786,6.916-2.128 c0.122,0.172,0.257,0.337,0.418,0.488l7.006,6.542c1.122,1.048,2.783,1.093,3.709,0.101 C406.327,636.507,406.169,634.853,405.047,633.805z M387.351,629.051c-4.893,0-8.86-3.967-8.86-8.86s3.967-8.86,8.86-8.86 s8.86,3.967,8.86,8.86S392.244,629.051,387.351,629.051z"/>
+						</svg>
+					</a>
+		    </div>
+		    
+		    <input type="button" class="search-submit hidden" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
+		</form>
+		<div class="clear"></div>
+	</div>
+	<H1 class="bold-t" >FnB Circle News</H1>
+
 </div>
 <div class="wrap">
 <h3 class="bold-t">Featured News on FnB</h3>

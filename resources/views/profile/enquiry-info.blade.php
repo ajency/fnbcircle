892ee89ff9dc736f2cf4c61d76de7674d459766c
Ajaj Rajguru <ajaj@ajency.in>
2017-11-22 11:59:51 +0530

    #1375 - fixed

--- resources/views/profile/enquiry-info.blade.php
+++ resources/views/profile/enquiry-info.blade.php
@@ -33,7 +33,7 @@
                                     </ul>
                                     <div class="tab-content">
                                         <div class="tab-pane active" id="recent-activity" role="tabpanel">
-                                            <div id="load-more-container"><button type="button" id="load-more-action">View More</button></div>
+                                            <div id="load-more-container" class="text-center"><button type="button" id="load-more-action" class="btn fnb-btn primary-btn border-btn posUpdate full">View More</button></div>
                                             <!-- <h6 class="enquiries-made title">
                                                 <i aria-hidden="true" class="fa fa-comments">
                                                 </i>

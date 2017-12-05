6b27c4d10e438f8feb406eaa89c5442cc18f5481
Shashank Shetye Saudagar <shashank@ajency.in>
2017-12-05 18:07:48 +0530

    #1499

--- resources/views/add-listing/business-info.blade.php
+++ resources/views/add-listing/business-info.blade.php
@@ -168,7 +168,7 @@
         <div class="contact-row m-t-5">
             <div class="row contact-container internal-contact-row">
                 <div class="col-sm-5">
-                    <input name="user-phone" class="form-control fnb-input p-l-5 p-t-0 contact-input contact-mobile-input contact-mobile-number" type="tel" placeholder="User Contact" class="form-control fnb-input p-l-5" value="@if($listing->owner_id != null){{$owner->getPrimaryContact()['contact']}}@endif"   @if($owner->type == 'external') readonly=""  @endif data-intl-country="{{$owner->getPrimaryContact()['contact_region']}}" >
+                    <input name="user-phone" class="form-control fnb-input p-l-5 p-t-0 contact-mobile-input contact-mobile-number" type="tel" placeholder="User Contact" class="form-control fnb-input p-l-5" value="@if($listing->owner_id != null){{$owner->getPrimaryContact()['contact']}}@endif"   @if($owner->type == 'external') readonly=""  @endif data-intl-country="{{$owner->getPrimaryContact()['contact_region']}}" >
                     <input type="hidden" class="contact-country-code" name="contact_country_code[]" @if($owner->type == 'external')  value="{{$owner->getPrimaryContact()['contact_region']}}" @endif>
                 </div>
                 <div class="col-sm-3 col-xs-4">

ca96cefad7407d4f8f6661d5cd8cc3e06e6ab81d
Ajaj Rajguru <ajaj@ajency.in>
2017-12-12 10:02:33 +0530

    job rejected email styled

--- resources/views/email/job-rejected.blade.php
+++ resources/views/email/job-rejected.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.email')
 
 @section('content')
-<table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
-    <tr style="text-align: center;">
+     <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
+    <tr style="text-align: center;background-color: #fff;">
       <td style="padding: 30px; font-family: sans-serif; font-size: 16px; line-height: 24px; color: #555555;">
   
 
         <div style="color: rgba(123, 123, 123, 0.77);font-weight: 300;">
-          <p>Hi <b style="color: #7f7f7f;"> {{ $ownerName}}</b></p>
+          <p>Hi <b style="color: #7f7f7f;"> {{ $ownerName}}</b>,</p>
          
           
         </div>
@@ -26,7 +26,7 @@
               
               We regret to inform you that your job <a href="{{ url('/job/'.$job->getJobSlug()) }}" style="color: #ec6d4b;" target="_blank">{{ $job->title }}</a> which was sent for review has been rejected by our team.<br><br>
 
-              <div style="font-weight: 600;"><span style="color: #27b7b0; font-weight: 600;">NOTE:</span> Rejected jobs can be submitted for review again.</div><br>
+              <div style=""><span style="font-weight: 600;">NOTE:</span> Rejected jobs can be submitted for review again.</div><br>
 
               Please contact us for any queries or concerns.<br><br>
 

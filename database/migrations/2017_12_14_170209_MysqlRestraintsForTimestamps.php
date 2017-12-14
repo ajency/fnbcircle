c62adef6b595a1f3f6021376fc04da06761406a7
Shashank Shetye Saudagar <shashank@ajency.in>
2017-12-14 17:35:06 +0530

    mysql constraint for timestamps

---
+++ database/migrations/2017_12_14_170209_MysqlRestraintsForTimestamps.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MysqlRestraintsForTimestamps extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement( 'ALTER TABLE  `users` CHANGE  `created_at`  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP' );
+        DB::statement( 'ALTER TABLE  `users` CHANGE  `updated_at`  `updated_at` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP' );
+        DB::statement( 'ALTER TABLE  `user_communications` CHANGE  `created_at`  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP' );
+        DB::statement( 'ALTER TABLE  `user_communications` CHANGE  `updated_at`  `updated_at` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP' );
+        DB::statement( 'ALTER TABLE  `listings` CHANGE  `created_at`  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP' );
+        DB::statement( 'ALTER TABLE  `listings` CHANGE  `updated_at`  `updated_at` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP' );
+        DB::statement( 'ALTER TABLE  `listing_category` CHANGE  `created_at`  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP' );
+        DB::statement( 'ALTER TABLE  `listing_category` CHANGE  `updated_at`  `updated_at` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP' );
+        DB::statement( 'ALTER TABLE  `listing_areas_of_operations` CHANGE  `created_at`  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP' );
+        DB::statement( 'ALTER TABLE  `listing_areas_of_operations` CHANGE  `updated_at`  `updated_at` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP' );
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        DB::statement( 'ALTER TABLE  `users` CHANGE  `created_at`  `created_at` TIMESTAMP NULL' );
+        DB::statement( 'ALTER TABLE  `users` CHANGE  `updated_at`  `updated_at` TIMESTAMP NULL' );
+        DB::statement( 'ALTER TABLE  `user_communications` CHANGE  `created_at`  `created_at` TIMESTAMP NULL' );
+        DB::statement( 'ALTER TABLE  `user_communications` CHANGE  `updated_at`  `updated_at` TIMESTAMP NULL' );
+        DB::statement( 'ALTER TABLE  `listings` CHANGE  `created_at`  `updated_at` TIMESTAMP NULL' );
+        DB::statement( 'ALTER TABLE  `listings` CHANGE  `updated_at`  `updated_at` TIMESTAMP NULL' );
+        DB::statement( 'ALTER TABLE  `listing_category` CHANGE  `created_at`  `updated_at` TIMESTAMP NULL' );
+        DB::statement( 'ALTER TABLE  `listing_category` CHANGE  `updated_at`  `updated_at` TIMESTAMP NULL' );
+        DB::statement( 'ALTER TABLE  `listing_areas_of_operations` CHANGE  `created_at`  `updated_at` TIMESTAMP NULL' );
+        DB::statement( 'ALTER TABLE  `listing_areas_of_operations` CHANGE  `updated_at`  `updated_at` TIMESTAMP NULL' );
+
+    }
+}

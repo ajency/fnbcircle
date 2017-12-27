04c47cefb1a341cb1dfe018a9724663ae27bf4c7
Ajaj Rajguru <ajaj@ajency.in>
2017-12-27 11:02:25 +0530

    #1606 - styled

--- resources/views/email/success_seeker_contactrequest_premium.blade.php
+++ resources/views/email/success_seeker_contactrequest_premium.blade.php
@@ -13,30 +13,31 @@
 				<div style="margin-bottom: 0.8em;">        
 					<div style="float: left;text-align: left;color: rgba(123, 123, 123, 0.77);font-size: 0.9em;">
 						Thank you for showing your interest in <a href="{{$listing_url}}">{{$listing->title}} </a>. We have shared your contact details with the owner of this listing. They would contact you soon.<br>
-Please find below contact details of the listing.<br>
-Listing Name: {{$listing->title}} <br>
+						
+						Please find below contact details of the listing.<br>
+						Listing Name: {{$listing->title}} <br>
 
-@php
-	$contacts = $listing->getAllContacts();
-@endphp
-@if(count($contacts['email']))				
-	Email:<br> 
-	@foreach($contacts['email'] as $email)
-		<a href="mailto:{{$email['email']}}"> {{$email['email']}} </a> @if($email['is_verified'])(verified) @else (unverified) @endif 
-	@endforeach
-@endif
-@if(count($contacts['mobile']))
-	Phone: <br>
-	@foreach($contacts['mobile'] as $mobile)
-		<a href="tel:{{$mobile['contact_region']}}{{$mobile['contact']}}"> +{{$mobile['contact_region']}}-{{$mobile['contact']}} </a> @if($mobile['is_verified'])(verified) @else (unverified) @endif <br>
-	@endforeach
-@endif
-@if(count($contacts['landline']))
-	Landline: <br>
-	@foreach($contacts['landline'] as $mobile)
-		<a href="tel:{{$mobile['contact_region']}}{{$mobile['contact']}}"> +{{$mobile['contact_region']}}-{{$mobile['contact']}} </a> @if($mobile['is_verified'])(verified) @else (unverified) @endif <br>
-	@endforeach	
-@endif
+						@php
+							$contacts = $listing->getAllContacts();
+						@endphp
+						@if(count($contacts['email']))				
+							Email:<br> 
+							@foreach($contacts['email'] as $email)
+								<a href="mailto:{{$email['email']}}"> {{$email['email']}} </a> @if($email['is_verified'])(verified) @else (unverified) @endif 
+							@endforeach
+						@endif
+						@if(count($contacts['mobile']))
+							Phone: <br>
+							@foreach($contacts['mobile'] as $mobile)
+								<a href="tel:{{$mobile['contact_region']}}{{$mobile['contact']}}"> +{{$mobile['contact_region']}}-{{$mobile['contact']}} </a> @if($mobile['is_verified'])(verified) @else (unverified) @endif <br>
+							@endforeach
+						@endif
+						@if(count($contacts['landline']))
+							Landline: <br>
+							@foreach($contacts['landline'] as $mobile)
+								<a href="tel:{{$mobile['contact_region']}}{{$mobile['contact']}}"> +{{$mobile['contact_region']}}-{{$mobile['contact']}} </a> @if($mobile['is_verified'])(verified) @else (unverified) @endif <br>
+							@endforeach	
+						@endif
 
 						You may now contact the owner directly.
 						When you contact the listing, don't forget to mention that you found it on FnB Circle.

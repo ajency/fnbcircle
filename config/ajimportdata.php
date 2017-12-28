0684c621d0484a81cbf34d5b742807fc11cea516
Shashank Shetye Saudagar <shashank@ajency.in>
2017-12-28 15:45:14 +0530

    New configs

--- config/ajimportdata.php
+++ config/ajimportdata.php
@@ -67,6 +67,15 @@ $ajimport_config['fileheader'] = array(
         'Draft', 'Draft_val',
 );
 
+$ajimport_config['invalid_matches'] = array(
+    ['Email1','Email2'],
+    ['Mobile1','Mobile2'],
+    ['Landline1','Landline2'],
+    ['CoreCategory1_id','CoreCategory2_id','CoreCategory3_id','CoreCategory4_id','CoreCategory5_id','CoreCategory6_id','CoreCategory7_id','CoreCategory8_id','CoreCategory9_id','CoreCategory10_id'],
+    ['Brand1_id','Brand2_id','Brand3_id','Brand4_id','Brand5_id','Brand6_id','Brand7_id','Brand8_id','Brand9_id','Brand10_id'],
+    ['AreaOfOperation1_id','AreaOfOperation2_id','AreaOfOperation3_id','AreaOfOperation4_id','AreaOfOperation5_id','AreaOfOperation6_id','AreaOfOperation7_id','AreaOfOperation8_id','AreaOfOperation9_id','AreaOfOperation10_id'],
+);
+
 /**
  * config to update any id column(for ex user_id) based on set of fields from child table(for ex user_communication table)
  */
@@ -356,5 +365,11 @@ $ajimport_config['childtables'][] = array(
 
 
 /* End Add Child tables here */
+
+$ajimport_config['aj_batchcallbacks'] = array(
+  array('function_name'=>'importCallback',
+        'class_path'   =>'\App\Http\Controllers\AdminModerationController',
+       ),         
+); 
 
 return $ajimport_config;

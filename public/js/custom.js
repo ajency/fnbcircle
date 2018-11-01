611261774227f181ca0c8039c03fc5c11be0b5e1
Amit <amit@ajency.in>
2018-11-01 18:33:18 +0530

    home page changes

--- public/js/custom.js
+++ public/js/custom.js
@@ -1280,4 +1280,47 @@ $(function(){
 
 });
 
+    $(document).ready(function(){
 
+$('.events-list').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+	
+$('.fnb-card-slider').slick({
+  slidesToShow: 1,
+  arrows:false,
+  slidesToScroll: 1,
+   dots: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+});
+	
